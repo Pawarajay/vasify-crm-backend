69fc049ff86331fa84a84b21cb386d4a8efcc593
Ajay Pawar <pawarajay7.ap@gmail.com>
2025-12-23 11:16:43 +0530

    Inital commit

---
+++ debug-reset-admin.js
@@ -0,0 +1,21 @@
+// // debug-reset-admin.js
+// const bcrypt = require("bcryptjs");
+// const { pool } = require("./config/database");
+
+// (async () => {
+//   try {
+//     const plain = "Admin@123"; // new known password
+//     const hash = await bcrypt.hash(plain, 10);
+
+//     await pool.execute(
+//       "UPDATE users SET password = ? WHERE email = ?",
+//       [hash, "admin@vasifytech.com"]
+//     );
+
+//     console.log("Password reset OK");
+//     process.exit(0);
+//   } catch (e) {
+//     console.error(e);
+//     process.exit(1);
+//   }
+// })();
