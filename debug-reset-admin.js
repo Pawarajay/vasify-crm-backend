705483f59806a1c55261cd03d419e5a322525aa7
Ajay Pawar <pawarajay7.ap@gmail.com>
2025-12-26 10:08:02 +0530

    New changes to fix the database issue

--- debug-reset-admin.js
+++ debug-reset-admin.js
@@ -1,21 +1,21 @@
-// // debug-reset-admin.js
-// const bcrypt = require("bcryptjs");
-// const { pool } = require("./config/database");
+// debug-reset-admin.js
+const bcrypt = require("bcryptjs");
+const { pool } = require("./config/database");
 
-// (async () => {
-//   try {
-//     const plain = "Admin@123"; // new known password
-//     const hash = await bcrypt.hash(plain, 10);
+(async () => {
+  try {
+    const plain = "Admin@123"; // new known password
+    const hash = await bcrypt.hash(plain, 10);
 
-//     await pool.execute(
-//       "UPDATE users SET password = ? WHERE email = ?",
-//       [hash, "admin@vasifytech.com"]
-//     );
+    await pool.execute(
+      "UPDATE users SET password = ? WHERE email = ?",
+      [hash, "admin@vasifytech.com"]
+    );
 
-//     console.log("Password reset OK");
-//     process.exit(0);
-//   } catch (e) {
-//     console.error(e);
-//     process.exit(1);
-//   }
-// })();
+    console.log("Password reset OK");
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
