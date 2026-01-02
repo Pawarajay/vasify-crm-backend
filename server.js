050ca2dd3dd58663ae240c77aa9b595229c17606
Ajay Pawar <pawarajay7.ap@gmail.com>
2026-01-02 20:32:55 +0530

    Updating frontend link

--- server.js
+++ server.js
@@ -22,11 +22,12 @@ const projectRoutes = require("./routes/projects");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const WEBSITE_URL = "https://vasifytech.com";
-const FRONTEND_URL = (process.env.FRONTEND_URL || "http://localhost:3000").replace(/\/$/, '');
+const FRONTEND_URL = (process.env.FRONTEND_URL || " https://crm-new.vasifytech.com").replace(/\/$/, '');
 
 const allowedOrigins = [
   FRONTEND_URL,
   'http://localhost:3000',
+   'https://crm-new.vasifytech.com',
   WEBSITE_URL,
 ];
 
